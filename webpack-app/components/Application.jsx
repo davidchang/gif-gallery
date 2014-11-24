37d4667995d58398f59eb878ff013ee27dcc15fd
David Chang <davidc@hasoffers.com>
2014-11-23 19:33:31 -0800

    upgrade to react 0.12, introduce react-router

--- webpack-app/components/Application.jsx
+++ webpack-app/components/Application.jsx
@@ -1,50 +1,32 @@
 var React = require('react');
 
-var galleryActions = require('actions/galleryActions');
-
 require('bootstrap/dist/css/bootstrap.css');
 require('styles/styles.less');
 
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var RouteHandler = Router.RouteHandler;
 
-require('stores/galleryStore');
+var CreateGallery = require('components/CreateGallery');
 
 var Application = React.createClass({
 
-  _createGallery : function() {
-    var inputData = {
-      'title'       : document.getElementById('title').value,
-      'description' : document.getElementById('description').value
-    };
-    return galleryActions.createGallery(inputData);
-  },
-
   render : function() {
     return (
-      <div className="page-wrapper">
-        <h1 className="text-center">Create a New Gif Gallery</h1>
-        <section className="form-horizontal" role="form">
-          <div className="form-group">
-            <label htmlFor="title" className="col-sm-1 control-label">Title</label>
-            <div className="col-sm-11">
-              <input type="text" className="form-control" id="title" />
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="description" className="col-sm-1 control-label">Description</label>
-            <div className="col-sm-11">
-              <textarea className="form-control" id="description"></textarea>
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-sm-offset-1 col-sm-11">
-              <button className="btn btn-default" onClick={this._createGallery}>Create</button>
-            </div>
-          </div>
-        </section>
-      </div>
+      <RouteHandler />
     );
   }
 
 });
 
-React.renderComponent(Application(), document.body);
+var routes = (
+  <Route name="app" path="/" handler={Application}>
+    <Route name="create" handler={CreateGallery} />
+    <DefaultRoute handler={CreateGallery} />
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler />, document.body);
+});
---
+++ webpack-app/components/CreateGallery.jsx
@@ -0,0 +1,40 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+  _createGallery : function() {
+    var inputData = {
+      'title'       : document.getElementById('title').value,
+      'description' : document.getElementById('description').value
+    };
+    return galleryActions.createGallery(inputData);
+  },
+
+  render : function() {
+    return (
+      <div className="page-wrapper">
+        <h1 className="text-center">Create a New Gif Gallery</h1>
+        <section className="form-horizontal" role="form">
+          <div className="form-group">
+            <label htmlFor="title" className="col-sm-1 control-label">Title</label>
+            <div className="col-sm-11">
+              <input type="text" className="form-control" id="title" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description" className="col-sm-1 control-label">Description</label>
+            <div className="col-sm-11">
+              <textarea className="form-control" id="description"></textarea>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-offset-1 col-sm-11">
+              <button className="btn btn-default" onClick={this._createGallery}>Create</button>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
+});
