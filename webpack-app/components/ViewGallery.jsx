5f2d52e167dc73a4f760cf62a9034c37f215e5e2
David Chang <davidc@hasoffers.com>
2014-11-25 22:49:53 -0800

    adding view gallery view

--- webpack-app/components/ViewGallery.jsx
+++ webpack-app/components/ViewGallery.jsx
@@ -2,11 +2,43 @@ var React = require('react');
 
 module.exports = React.createClass({
 
+  componentDidMount : function() {
+    var $gallery = document.getElementById('gallery');
+
+    var keys = Object.keys(data);
+    keys = keys.sort(function() { return 0.5 - Math.random() });
+    keys = keys.sort(function() { return 0.5 - Math.random() });
+    keys.forEach(key => {
+      var section = document.createElement('section');
+      section.className = 'section';
+
+      var img = document.createElement('img');
+      img.src = data[key].src;
+
+      var div = document.createElement('div');
+      div.innerHTML = data[key].message;
+
+      section.appendChild(img);
+      section.appendChild(div);
+
+      $gallery.appendChild(section);
+    });
+
+    var msnry = new Masonry( $gallery, {
+      // options
+      columnWidth: 0,
+      itemSelector: '.section'
+    });
+  },
+
   render : function() {
     return (
-      <div className="page-wrapper">
-        <h1>View Gallery</h1>
-      </div>
+      <section className="gallery-container">
+        <div className="page-wrapper">
+          <h1>Everyone Who Said Hi on your Birthday!</h1>
+        </div>
+        <div id="gallery"></div>
+      </section>
     );
   }
 
