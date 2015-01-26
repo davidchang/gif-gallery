7284a26a3023bec255dafbeb12b332fa132ae3fa
David Chang <davidc@hasoffers.com>
2015-01-25 19:56:19 -0800

    fixing the index path

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/', function(req, res) {
-    return res.sendFile('/client/index.html');
+    return res.sendFile(path.join(__dirname, './../../client', 'index.html'));
   });
   server.use(router);
 };
